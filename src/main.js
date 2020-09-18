637b3940fab8e07eabec626f8d7ccd273fc3bc63
Roy Nijhof <roy@showntelldata.com>
2020-09-18 08:57:31 -0500

    Exposed the Collapsible component in the main.js file as well

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Accordion from './components/Accordion.vue';
 import AccordionList from './components/AccordionList.vue';
+import Collapsible from './components/Collapsible.vue';
 
 export {
+  Collapsible,
   Accordion,
   AccordionList
 };
